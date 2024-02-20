8f0bf52053f21492d5ef3298c1218553ae607629
hongyi-zhao <hongyi.zhao@gmail.com>
2024-02-20 20:42:53 +0800

    Update direct_to_cart_vasp_poscar.py

--- direct_to_cart_vasp_poscar.py
+++ direct_to_cart_vasp_poscar.py
@@ -6,23 +6,23 @@ Created on Tues Mar 28 13:56:18 2023
 """
 
 from pymatgen.core import Structure
+
+# Load the structure from POSCAR
 structure = Structure.from_file("POSCAR")
 
-f1 = open('POSCAR','r')
-f2 = open('POSCAR_cart','w')
+# Open the original POSCAR to read header information
+# and the new POSCAR_Cart file to write the modified structure
+with open('POSCAR', 'r') as f1, open('POSCAR_cart', 'w') as f2:
+    # Copy the header from the original POSCAR: first 7 lines
+    for _ in range(7):
+        f2.write(f1.readline())
 
-i=0
-while True:
-    line = f1.readline()
-    i+=1
-    if i < 8:
-        f2.write(line)    
-    if i == 8:
-        f2.writelines('Cartesian \n')
-    if i > 8:
-        break    
-f1.close()
+    # Write the line indicating that the coordinates following are in Cartesian format
+    f2.write('Cartesian\n')
 
-for i in range(structure.num_sites):
-    f2.writelines("{:14f} {:14f} {:14f}\n".format(structure.cart_coords[i][0],structure.cart_coords[i][1],structure.cart_coords[i][2]))  
-f2.close()
+    # Write the Cartesian coordinates for each site
+    for site in structure:
+        coords = site.coords  # pymatgen already handles coordinates as Cartesian by default in the Structure object
+        f2.write("{:14f} {:14f} {:14f}\n".format(coords[0], coords[1], coords[2]))
+
+print("Conversion complete. Cartesian coordinates saved in POSCAR_cart.")
